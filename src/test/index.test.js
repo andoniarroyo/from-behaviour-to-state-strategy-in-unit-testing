867782d57f8c733719b98e8caf25b7d4c8960308
Andoni Arroyo <andoni.arroyo@williamhill.com>
2017-12-03 01:45:08 +0100

    Improves the checking by state

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,14 +1,4 @@
-import transformType1 from '../main/transformers/type1Transformer';
-import transformType2 from '../main/transformers/type2Transformer';
 import transform from '../main';
-
-jest.mock('../main/transformers/type1Transformer');
-jest.mock('../main/transformers/type2Transformer');
-
-beforeEach(() => {
-  transformType1.mockReset();
-  transformType2.mockReset();
-});
 
 describe('index.js', () => {
   it('transforms the incoming raw object', () => {
@@ -17,27 +7,20 @@ describe('index.js', () => {
       content: [
         {
           type: 'type1',
-          value: 'prop1',
+          value: 'PROP1',
         },
         {
           type: 'type2',
-          value: 'PROP2',
+          value: 'prop2',
         },
       ],
     };
-
-    transformType1.mockImplementation(jest.fn(() => 'fakeType1Transformation'));
-    transformType2.mockImplementation(jest.fn(() => 'fakeType2Transformation'));
 
     const result = transform(rawObject);
 
     // checking by state
     expect(result.name).toBe('name');
-    expect(result.content[0].value).toBe('fakeType1Transformation');
-    expect(result.content[1].value).toBe('fakeType2Transformation');
-
-    // checking by behaviour
-    expect(transformType1).toBeCalledWith(rawObject.content[0]);
-    expect(transformType2).toBeCalledWith(rawObject.content[1]);
+    expect(result.content[0].value).toBe('prop1');
+    expect(result.content[1].value).toBe('PROP2');
   });
 });
